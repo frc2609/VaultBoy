6d977f6504581f00dda26175c52e4b5ae7f0f0bc
student <student@DESKTOP-QINJEGB>
2018-04-05 14:48:24 -0400

    teleop logging prep

--- src/org/usfirst/frc/team2609/MP/Logger.java
+++ src/org/usfirst/frc/team2609/MP/Logger.java
@@ -1,8 +1,11 @@
 package org.usfirst.frc.team2609.MP;
 import java.io.*;
 
+import org.usfirst.frc.team2609.robot.OI;
 import org.usfirst.frc.team2609.robot.Robot;
 import org.usfirst.frc.team2609.robot.RobotMap;
+import org.usfirst.frc.team2609.robot.commands.automation.CubeCollect;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -65,7 +68,9 @@ public class Logger {
     public void openFile() {
     	if(this.wantToLog() || this.ds.isFMSAttached()){
 	        try{
-	            path = this.getPath();
+	        	path = this.getPath();
+	            
+	            
 	            this.writer = new BufferedWriter(new FileWriter(path));
 	            this.writer.write("FPGATime, encLeft, encRight, velLeft, velRight, leftCurrent, rightCurrent, leftVoltage, rightVoltage, leftEncSetp, rightEncSetp, leftVelSetp, rightVelSetp, leftAccelSetp, rightAccelSetp, gyroSetp, yaw, angle, ");
 	            this.writer.write(String.format("%.3f", (double)MPConstants.cruiseVelocity));
@@ -90,7 +95,8 @@ public class Logger {
 	        try{
 	            path = this.getPath();
 	            this.writer = new BufferedWriter(new FileWriter(path));
-	            this.writer.write("FPGATime, shooterL, shooterR, shooterVoltageL, shooterVoltageR, vaultboyL, vaultboyR, vaultboyVoltageL, vaultboyVoltageR");
+	            this.writer.write("FPGATime, leftDriveVolt, rightDriveVolt, vaultVolt, leftIntakeVolt, rightIntakeVolt, sliderVolt, sliderPos, cubeSensor, cubeArmSensor");
+	            
 	            this.writer.newLine();
 	        } catch (IOException e) {
 	            e.printStackTrace();
@@ -99,7 +105,7 @@ public class Logger {
     }
     
     private String getPath() {
-    	this.fileName = SmartDashboard.getString(SDFileName, "beaverlog");
+    	this.fileName = (ds.isOperatorControl()) ? SmartDashboard.getString(SDFileName, "beaverlog")+"_TELE" : SmartDashboard.getString(SDFileName, "beaverlog");
         if(this.ds.isFMSAttached()) {
             return String.format("/home/lvuser/beaverlogs/%d_%s_%d_log.csv", ++this.max, this.ds.getAlliance().name(), this.ds.getLocation());
         }else if(this.fileName != null){ 
@@ -108,6 +114,8 @@ public class Logger {
             return String.format("/home/lvuser/beaverlogs/%d_log.csv", ++this.max);
         }
     }
+    
+    
     public void logTele() {
     	if(this.wantToLog()){
 	        try {
